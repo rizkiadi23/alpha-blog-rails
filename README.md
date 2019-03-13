4a291a443bb171a15b9ccb3940ca35cfa37ca4cf
Rizki Adi Nugroho <rizkiadinugroho23@gmail.com>
2019-03-14 01:29:05 +0700

    add user validations

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Simple Rails Alpha Blog
 Work in progress...
+
+# Use Cases
+## User Validation
+- username must be present and unique
+- email must be present and unique
+- validate email format using regex
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :username, presence: true, 
+                       length: { minimum: 5, maximum: 25 }, 
+                       uniqueness: { case_sensitive: false }
+
+  # Check it out at rubular.com
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+                    length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end

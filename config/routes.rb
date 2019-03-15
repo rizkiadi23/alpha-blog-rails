cc82b7612c18bc19128d538524b6007211cfab5e
Rizki Adi Nugroho <rizkiadinugroho23@gmail.com>
2019-03-16 01:24:36 +0700

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  "pages#home"
   get   "pages/about" => "pages#about"
+
   resources :articles
+
   get 'signup' => 'users#new'
   resources :users, except: [:new]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end

3801cdbe96f878c37c6efd229c5e96a2a1934462
Rizki Adi Nugroho <rizkiadinugroho23@gmail.com>
2019-03-16 04:08:01 +0700

    Add negative test in create_categories_test.rb

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -16,4 +16,18 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
     assert_match "sports", response.body
   end
+
+  test "Invalid category submission results in failure" do
+    get new_category_path
+
+    assert_template 'categories/new'
+
+    assert_no_difference 'Category.count' do
+      post categories_path, params:{category:{name: " "}}
+    end
+
+    assert_template 'categories/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.alert.alert-danger.alert-dismissible.fade.show'
+  end
 end
